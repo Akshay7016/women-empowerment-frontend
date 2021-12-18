cbb4ae634492f45a43ef2763cb21bfd99b781947
Akshay Sajgule <asajgule@gmail.com>
2021-12-18 10:35:37 +0530

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+import SchemeComponent from "./components/SchemeComponent";
+
+
+
+test('render Data from Scheme Component', () => {
+  render(
+    <Provider store={store} >
+      <SchemeComponent />
+    </Provider>);
+  const linkElement = screen.getByText('Scheme Component');
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('render Data from Scheme Component', () => {
+  render(
+    <Provider store={store} >
+      <SchemeComponent />
+    </Provider>);
+  const linkElement = screen.getByText('Find scheme by Id');
+  expect(linkElement).toBeInTheDocument();
+});
+
